efd7ad308c58d618b4da8d6540784cc55c7beeec
dennis2110 <dennis2110@hotmail.com.tw>
2019-05-04 16:15:17 +0800

    change demo_pub to num count

--- demo_pkg/src/demo_pub.cpp
+++ demo_pkg/src/demo_pub.cpp
@@ -1,21 +1,25 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "std_msgs/Int16.h"
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "demo_pub");
   ros::NodeHandle nh;
 
-  ros::Publisher chatter_pub = nh.advertise<std_msgs::String>("chatter", 1000);
+  ros::Publisher chatter_pub = nh.advertise<std_msgs::Int16>("/numcount", 1000);
 
+  int16_t count = 0;
   ros::Rate loop_rate(10);
   while (ros::ok())
   {
+    std_msgs::Int16 intmsg;
+    intmsg.data = count;
     std_msgs::String msg;
     msg.data = "hello world";
 
-    chatter_pub.publish(msg);
-
+    chatter_pub.publish(intmsg);
+    count +=1;
     ros::spinOnce();
 
     loop_rate.sleep();
